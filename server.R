79bc08aa2110fe7abb89cd844f70d61aa5f36fd2
ssushmitha <41013333+ssushmitha@users.noreply.github.com>
2018-07-18 19:08:17 +0530

    Update server.R

--- server.R
+++ server.R
@@ -5,26 +5,15 @@ library(tseries)
 
 
 shinyServer(function(input, output){
-  #########################################################
-  select.product <- reactive({                                  # function that will filter that product based on the user's input
-    product_data <- data[data$PRODUCT_ID == input$product_id,] 
-    return(product_data = product_data)
-  }) 
   
+  select.product <- reactive({                                  # function that will filter that product based on the user's input
+  product_data <- data[data$PRODUCT_ID == input$product_id,] 
+  return(product_data = product_data)
+  }) 
 
   output$product_dataHead <- renderDataTable(select.product())
-  
-  
-  
-  output$progressBox <- renderValueBox({
-    valueBox(
-      paste0(25 + input$count, "%"), "Progress", icon = icon("list"),
-      color = "purple"
-    )
+  )
   })
-  
- # output$summarymonthplot4 = renderDataTable(Channelpath,options=list(dom="t"))
-  
 })
 
 
