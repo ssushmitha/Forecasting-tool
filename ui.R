3098b6ec65721c7f9a0d3b392abf3dba502b030d
ssushmitha <41013333+ssushmitha@users.noreply.github.com>
2018-07-18 19:16:08 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -6,88 +6,81 @@ library(shinythemes)
 
 
 data <- read.csv("D:/Projects/Demand Forecasting/Sathish model/07182018_V2/salestable.csv", stringsAsFactors = FALSE)
-dataChoices <- c(data$Product_category)
-
+#dataChoices <- c(data$Product_category)
 
 
 # Define UI logic required for prediction
 shinyUI(
   
   dashboardPage(skin = c("green"),
-    dashboardHeader(title = "Demand Forecasting",
-                    tags$li(a(href = 'http://www.i2decisions.com',
-                              img(src = "i2Decisionslogo.png",
-                                  title = "Company Home", height = "30px"),
-                              style = "padding-top:10px; padding-bottom:10px;"),
+  dashboardHeader(title = "Demand Forecasting",
+                  tags$li(a(href = 'http://www.i2decisions.com',
+                            img(src = "i2Decisionslogo.png",
+                            title = "Company Home", height = "30px"),
+                            style = "padding-top:10px; padding-bottom:10px;"),
                             class = "dropdown")),
     
     
-    dashboardSidebar(
+  dashboardSidebar(
       
-      sidebarMenu(
-        selectInput(inputId = "product_id",
-                    "Category",
-                  #  choices = dataChoices,
-                  choices = c("Accessories","Bikes","Clothing"),
-                    selected = "Accessories"),
+  sidebarMenu(
+              selectInput(inputId = "Product_id "
+                          "Category",
+                       #  choices = dataChoices,
+                          choices = c("Accessories","Bikes","Clothing"),
+                          selected = "Accessories"),
         
         
-        dateRangeInput("dateRange",
-                       "Date range",
-                       start = "1980-01-01",
-                       end   = "2016-12-31")
-      ),
+              dateRangeInput("dateRange",
+                          "Date range",
+                           start = "1980-01-01",
+                           end   = "2016-12-31")),
       
 
-      selectInput("frequency",
-                  "Frequency",
-                  choices = c("Days" = "Daily","Weeks" = "Weekly", "Months" = "Monthly"), 
-                  selected = "Months"),
+              selectInput("Frequency",
+                          "Frequency",
+                           choices = c("Days" = "Daily","Weeks" = "Weekly", "Months" = "Monthly"), 
+                           selected = "Months"),
       
-      radioButtons("method",( "Forecast Technique: "), 
-     
-                   c("Naive" = "naive",
-                     "Moving Average" = "ma",
-                     "Exponential Smoothing" = "es"))
-
-    ),
+              radioButtons("method",( "Forecast Technique: "), 
+                          c("Naive" = "naive",
+                            "Moving Average" = "ma",
+                            "Exponential Smoothing" = "es"))),
     
     dashboardBody(
-      fluidRow(
-        valueBoxOutput("progressBox"),
-        tabBox(
-          title = "First tabBox",
-          # The id lets us use input$tabset1 on the server to find the current tab
-          id = "tabset1", height = "250px",
-          tabPanel("Tab1", "First tab content"),
-          tabPanel("Tab2", "Tab content 2")
-        ),
-        tabBox(
-          side = "right", height = "250px",
-          selected = "Tab3",
-          tabPanel("Tab1", "Tab content 1"),
-          tabPanel("Tab2", "Tab content 2"),
-          tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
-        )
-      ),
-      fluidRow(
-     
-        tabBox(
-          # Title can include an icon
-          title = tagList(shiny::icon("gear"), "tabBox status"),
-          tabPanel("Tab1",
-                   "Currently selected tab from first box:",
-                   verbatimTextOutput("tabset1Selected")
-          ),
-          tabPanel("Tab2", "Tab content 2")
-        )
-    
-      ),
+             fluidRow(
+      
+             tabBox(
+             title = "First tabBox",
+             # The id lets us use input$tabset1 on the server to find the current tab
+             id = "tabset1", height = "250px",
+             tabPanel("Tab1", "First tab content"),
+             tabPanel("Tab2", "Tab content 2")
+              ),
+               
+             tabBox(
+             side = "right", height = "250px",
+             selected = "Tab3",
+             tabPanel("Tab1", "Tab content 1"),
+             tabPanel("Tab2", "Tab content 2"),
+             tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
+             )
+             ),
+      
+             fluidRow(
+             tabBox(
+             # Title can include an icon
+             title = tagList(shiny::icon("gear"), "tabBox status"),
+             tabPanel("Tab1",
+             "Currently selected tab from first box:",
+              verbatimTextOutput("tabset1Selected")
+              ),
+              tabPanel("Tab2", "Tab content 2")
+              )
+              ),
 
-      
-      
-    tabItem(
-    box(dataTableOutput("product_dataHead"),width = 250)
+             tabItem(
+             box(dataTableOutput("product_dataHead"),width = 250)
       )
       
       ))
